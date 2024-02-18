0a5f77921d66f39726c5f5bb6c378e76cc6fe91b
Tomislav Dukez <tomdu3@ymail.com>
2024-02-18 20:03:00 +0000

    Add react-app setup files

---
+++ src/Message.jsx
@@ -0,0 +1,16 @@
+function Message() {
+    const name = 'Tomislav';
+    if (name)
+        return (
+            <>
+                <h1>Hello {name}</h1>
+            </>
+        );
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    );
+}
+
+export default Message;
